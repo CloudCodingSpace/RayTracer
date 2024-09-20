31581b97db1a3f7e9925a8f22dc2511551740b1c
CloudCodingSpace <subhamaysaha11@gmail.com>
2024-09-20 14:25:57 +0530

    Created the renderer and it outputs and png file!

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,10 @@
-#include <iostream>
+#include "RayTracer/RayTracer.h"
 
 int main(int argc, const char** argv) {
-    std::clog << "Setup Done!" << std::endl;
+    RayTracer rt;
+    rt.Init();
+    rt.Run();
+    rt.Cleanup();
 
     return 0;
 }
